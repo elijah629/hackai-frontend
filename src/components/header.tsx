a487fc0ef4d4a38b7e9285bea297f945839b2c06
Eli Ozcan <62805599+elijah629@users.noreply.github.com>
2025-11-29 08:48:17 +0000

    fix: mobile new chat button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,31 +1,25 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Edit, Menu } from "lucide-react";
 import Image from "next/image";
 import orpheus from "@/images/flag-orpheus-top.svg";
-import { useRouter } from "next/navigation";
-import { nanoid } from "nanoid";
+import { createChat } from "@/lib/db/actions";
 
 export function Header() {
-  const router = useRouter();
-
   return (
     <header className="border-b md:hidden items-center px-2 flex justify-between">
       <SidebarTrigger className="size-9">
         <Menu className="size-6" />
       </SidebarTrigger>
       <Image alt="Hack club" width={103} height={58} src={orpheus.src} />
-      <Button
-        variant="ghost"
-        size="icon-sm"
-        onClick={() => {
-          router.push(`/c/${nanoid()}`);
-        }}
-      >
-        <Edit className="size-6" />
-      </Button>
+      <Form action={createChat}>
+        <Button
+          variant="ghost"
+          size="icon-sm"
+        >
+          <Edit className="size-6" />
+        </Button>
+      </Form>
     </header>
   );
 }
